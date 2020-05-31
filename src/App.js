fc6877d0b4b7534e5985c2099d27eee70173745a
Leandro <quenochetete1120@gmail.com>
2020-05-31 19:05:20 -0300

    Inputs detectados y funcionando

--- src/App.js
+++ src/App.js
@@ -1,26 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Generator from "./Generator";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// que tiene que tener para crear una contraseña?
+// checkbox de selecion de Min, Mayus, Num y Caracteres
+// Cant caracteres
+class App extends React.Component {
+  state = {
+    min: {
+      abc: ["a", "b", "c"],
+      value: true,
+    },
+    mayus: {
+      ABC: ["A", "B", "C"],
+      value: true,
+    },
+    num: {
+      numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      value: true,
+    },
+    caracter: 20,
+  };
+
+  valueInputs = (e) => {
+    // this.setState({
+    //   min: { value: e.target.checked },
+    //   mayus: { value: e.target.checked },
+    //   num: { value: e.target.checked },
+    // });
+    console.log(this.state.min.value);
+    console.log(this.state.mayus.value);
+    console.log(this.state.num.value);
+  };
+
+  handleCheckboxMin = (e) => {
+    this.setState({
+      min: { value: e.target.checked },
+    });
+  };
+  handleCheckboxMayus = (e) => {
+    this.setState({
+      mayus: { value: e.target.checked },
+    });
+  };
+  handleCheckboxNum = (e) => {
+    this.setState({
+      num: { value: e.target.checked },
+    });
+  };
+
+  handleRangeChange = (e) => {
+    this.setState({ caracter: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Generator
+          min={this.state.min.value ? this.state.min.abc : null}
+          mayus={this.state.mayus.value ? this.state.mayus.ABC : null}
+          num={this.state.num.value ? this.state.num.numbers : null}
+          caracter={this.state.caracter}
+        />
+        <label>
+          <input
+            name="Minuscula"
+            type="checkbox"
+            checked={this.state.min.value}
+            onChange={this.handleCheckboxMin}
+          ></input>
+          Minuscula
+        </label>
+        <label>
+          <input
+            name="Mayuscula"
+            type="checkbox"
+            checked={this.state.mayus.value}
+            onChange={this.handleCheckboxMayus}
+          ></input>
+          Mayuscula
+        </label>
+        <label>
+          <input
+            name="Numeros"
+            type="checkbox"
+            checked={this.state.num.value}
+            onChange={this.handleCheckboxNum}
+          ></input>
+          Numeros
+        </label>
+        <label>
+          <input name="Caracteres" type="checkbox"></input>Caracteres
+        </label>
+        <label>
+          <input
+            id="typeinp"
+            type="range"
+            min="20"
+            max="50"
+            value={this.state.caracter}
+            onChange={this.handleRangeChange}
+            step="1"
+          />
+          {this.state.caracter}
+        </label>
+
+        <input type="button" value="Generar" onClick={this.valueInputs}></input>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Generator.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Generator({ min, mayus, num, caracter }) {
+  var password = new Array(caracter);
+
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+  }
+
+  return <h1>{password}</h1>;
+}
+export default Generator;
