fd146818067c7800a131780d43d3999924e2735f
Leandro <quenochetete1120@gmail.com>
2020-06-01 12:39:34 -0300

    Cambio en el sistema generacion

--- src/App.js
+++ src/App.js
@@ -7,103 +7,110 @@ import "./App.css";
 // Cant caracteres
 class App extends React.Component {
   state = {
-    min: {
-      abc: [
-        "a",
-        "b",
-        "c",
-        "d",
-        "e",
-        "f",
-        "g",
-        "h",
-        "i",
-        "j",
-        "k",
-        "l",
-        "m",
-        "n",
-        "ñ",
-        "o",
-        "p",
-        "q",
-        "r",
-        "s",
-        "t",
-        "u",
-        "v",
-        "w",
-        "x",
-        "y",
-        "z",
-      ],
-      value: true,
-    },
-    mayus: {
-      ABC: [
-        "A",
-        "B",
-        "C",
-        "D",
-        "E",
-        "F",
-        "G",
-        "H",
-        "I",
-        "J",
-        "K",
-        "L",
-        "M",
-        "N",
-        "Ñ",
-        "O",
-        "P",
-        "Q",
-        "R",
-        "S",
-        "T",
-        "U",
-        "V",
-        "W",
-        "X",
-        "Y",
-        "Z",
-      ],
-      value: true,
-    },
-    num: {
-      numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-      value: true,
-    },
+    abc: [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "ñ",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+    ],
+    ABC: [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "Ñ",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+    ],
+    numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
     caracter: 20,
+    password: [],
+    default: true,
   };
 
-  valueInputs = (e) => {
-    // this.setState({
-    //   min: { value: e.target.checked },
-    //   mayus: { value: e.target.checked },
-    //   num: { value: e.target.checked },
-    // });
+  onSubmit = (e) => {
+    e.preventDefault();
 
-    console.log(this.state.min.value);
-    console.log(this.state.mayus.value);
-    console.log(this.state.num.value);
-  };
+    const valueMin = e.target.minuscula.checked;
+    const valueMayus = e.target.mayuscula.checked;
+    const valueNum = e.target.numeros.checked;
 
-  handleCheckboxMin = (e) => {
-    this.setState({
-      min: { value: e.target.checked },
-    });
-  };
-  handleCheckboxMayus = (e) => {
-    this.setState({
-      mayus: { value: e.target.checked },
-    });
-  };
-  handleCheckboxNum = (e) => {
-    this.setState({
-      num: { value: e.target.checked },
-    });
+    if (valueMin && valueMayus && valueNum) {
+      this.setState({
+        password: [].concat(this.state.abc, this.state.ABC, this.state.numbers),
+      });
+    } else if (valueMin && valueMayus && valueNum === false) {
+      this.setState({
+        password: [].concat(this.state.abc, this.state.ABC),
+      });
+    } else if (valueMin && valueMayus === false && valueNum === false) {
+      this.setState({
+        password: [].concat(this.state.abc),
+      });
+    } else if (valueMin === false && valueMayus && valueNum) {
+      this.setState({
+        password: [].concat(this.state.ABC, this.state.numbers),
+      });
+    } else if (valueMin === false && valueMayus && valueNum === false) {
+      this.setState({
+        password: [].concat(this.state.ABC),
+      });
+    } else if (valueMin && valueMayus === false && valueNum) {
+      this.setState({
+        password: [].concat(this.state.abc, this.state.numbers),
+      });
+    } else if (valueMin === false && valueMayus && valueNum) {
+      this.setState({
+        password: [].concat(this.state.ABC, this.state.numbers),
+      });
+    } else if (valueMin === false && valueMayus === false && valueNum) {
+      this.setState({
+        password: [].concat(this.state.numbers),
+      });
+    }
   };
 
   handleRangeChange = (e) => {
@@ -114,55 +121,41 @@ class App extends React.Component {
     return (
       <div className="App">
         <Generator
-          min={this.state.min.value ? this.state.min.abc : null}
-          mayus={this.state.mayus.value ? this.state.mayus.ABC : null}
-          num={this.state.num.value ? this.state.num.numbers : null}
-          caracter={this.state.caracter}
+          valores={this.state.password}
+          cantidad={this.state.caracter}
         />
-        <label>
-          <input
-            name="Minuscula"
-            type="checkbox"
-            checked={this.state.min.value}
-            onChange={this.handleCheckboxMin}
-          ></input>
-          Minuscula
-        </label>
-        <label>
-          <input
-            name="Mayuscula"
-            type="checkbox"
-            checked={this.state.mayus.value}
-            onChange={this.handleCheckboxMayus}
-          ></input>
-          Mayuscula
-        </label>
-        <label>
-          <input
-            name="Numeros"
-            type="checkbox"
-            checked={this.state.num.value}
-            onChange={this.handleCheckboxNum}
-          ></input>
-          Numeros
-        </label>
-        <label>
-          <input name="Caracteres" type="checkbox"></input>Caracteres
-        </label>
-        <label>
-          <input
-            id="typeinp"
-            type="range"
-            min="1"
-            max="50"
-            value={this.state.caracter}
-            onChange={this.handleRangeChange}
-            step="1"
-          />
-          {this.state.caracter}
-        </label>
+        <form onSubmit={this.onSubmit}>
+          <label>
+            <input name="minuscula" type="checkbox"></input>
+            abc
+          </label>
+          <label>
+            <input name="mayuscula" type="checkbox"></input>
+            ABC
+          </label>
+          <label>
+            <input name="numeros" type="checkbox"></input>
+            123
+          </label>
+          <label>
+            <input name="Caracteres" type="checkbox"></input>Caracteres
+          </label>
+          <label>
+            <input
+              id="typeinp"
+              type="range"
+              name="range"
+              min="1"
+              max="50"
+              value={this.state.caracter}
+              onChange={this.handleRangeChange}
+              step="1"
+            />
+            {this.state.caracter}
+          </label>
 
-        <input type="button" value="Generar" onClick={this.valueInputs}></input>
+          <button>Generar</button>
+        </form>
       </div>
     );
   }
--- src/Generator.jsx
+++ src/Generator.jsx
@@ -1,22 +1,13 @@
 import React from "react";
 
-//dos formas de seleccion de caracter para la password
-// 1ro : los meto en un array nuevo y de ahi se elije un nro random y de ahi se asignan al pass
-// 2da : dentro de un for y a su vez nro random para elegir el array de cada item
-// 3ro : Switch con los value de cada lista de items
+function Generator({ valores, cantidad }) {
+  const p = [];
 
-function Generator({ min, mayus, num, caracter }) {
-  const password = [];
-
-  const join = min.concat(mayus).concat(num);
-  console.log(join);
-
-  for (let i = 0; i < caracter; i++) {
-    const random = Math.floor(Math.random() * join.length) + 1;
-    password[i] = join[random];
+  for (let i = 0; i < cantidad; i++) {
+    const random = Math.floor(Math.random() * valores.length) + 1;
+    p[i] = valores[random];
   }
-  console.log(password.length);
 
-  return <h1>{password}</h1>;
+  return <h1>{p}</h1>;
 }
 export default Generator;
