11e5fdefd90d17409016044df1a6e4fde2d4305c
Leandro <quenochetete1120@gmail.com>
2020-06-02 11:16:33 -0300

    Se agrega los simbolos y se agrega un poco de estilo. Generador de contraseña es funcional

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import Generator from "./Generator";
+import styled from "styled-components";
 import "./App.css";
 
 // que tiene que tener para crear una contraseña?
 // checkbox de selecion de Min, Mayus, Num y Caracteres
 // Cant caracteres
+
+const ControlPanel = styled.div`
+  width: auto;
+  height: auto;
+  border: 1px solid black;
+  background-color: white;
+  margin: 6px;
+  padding: 6px;
+  justify-content: center;
+`;
+
 class App extends React.Component {
   state = {
     abc: [
@@ -66,54 +78,37 @@ class App extends React.Component {
       "Z",
     ],
     numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-
+    simbolos: [
+      "%",
+      "&",
+      "'",
+      "(",
+      "#",
+      ")",
+      "*",
+      "{",
+      "}",
+      "[",
+      "]",
+      "+",
+      ",",
+      "-",
+      ".",
+      "/",
+      ":",
+      ";",
+      "<",
+      "=",
+      ">",
+      "?",
+      "@",
+    ],
     defaultMin: true,
     defaultMayus: true,
     defaultNum: true,
-
+    defaultSimbol: false,
     caracter: 20,
   };
-
-  // onSubmit = (e) => {
-  //   e.preventDefault();
-  //   const valueMin = this.state.defaultMin;
-  //   const valueMayus = this.state.defaultMayus;
-  //   const valueNum = this.state.defaultNum;
-
-  //   if (valueMin && valueMayus && valueNum) {
-  //     this.setState({
-  //       password: [].concat(this.state.abc, this.state.ABC, this.state.numbers),
-  //     });
-  //   } else if (valueMin && valueMayus && valueNum === false) {
-  //     this.setState({
-  //       password: [].concat(this.state.abc, this.state.ABC),
-  //     });
-  //   } else if (valueMin && valueMayus === false && valueNum === false) {
-  //     this.setState({
-  //       password: [].concat(this.state.abc),
-  //     });
-  //   } else if (valueMin === false && valueMayus && valueNum) {
-  //     this.setState({
-  //       password: [].concat(this.state.ABC, this.state.numbers),
-  //     });
-  //   } else if (valueMin === false && valueMayus && valueNum === false) {
-  //     this.setState({
-  //       password: [].concat(this.state.ABC),
-  //     });
-  //   } else if (valueMin && valueMayus === false && valueNum) {
-  //     this.setState({
-  //       password: [].concat(this.state.abc, this.state.numbers),
-  //     });
-  //   } else if (valueMin === false && valueMayus && valueNum) {
-  //     this.setState({
-  //       password: [].concat(this.state.ABC, this.state.numbers),
-  //     });
-  //   } else if (valueMin === false && valueMayus === false && valueNum) {
-  //     this.setState({
-  //       password: [].concat(this.state.numbers),
-  //     });
-  //   }
-  //};
 
   handleMinChange = (e) => {
     this.setState({ defaultMin: e.target.checked });
@@ -125,6 +120,9 @@ class App extends React.Component {
   handleNumChange = (e) => {
     this.setState({ defaultNum: e.target.checked });
   };
+  handleSimbolChange = (e) => {
+    this.setState({ defaultSimbol: e.target.checked });
+  };
 
   handleRangeChange = (e) => {
     this.setState({ caracter: e.target.value });
@@ -133,53 +131,58 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Generator valores={this.state} />
-
-        <label>
-          <input
-            name="minuscula"
-            type="checkbox"
-            checked={this.state.defaultMin}
-            onChange={this.handleMinChange}
-          ></input>
-          abc
-        </label>
-        <label>
-          <input
-            name="mayuscula"
-            type="checkbox"
-            checked={this.state.defaultMayus}
-            onChange={this.handleMayusChange}
-          ></input>
-          ABC
-        </label>
-        <label>
-          <input
-            name="numeros"
-            type="checkbox"
-            checked={this.state.defaultNum}
-            onChange={this.handleNumChange}
-          ></input>
-          123
-        </label>
-        <label>
-          <input name="Caracteres" type="checkbox"></input>Caracteres
-        </label>
-        <label>
-          <input
-            id="typeinp"
-            type="range"
-            name="range"
-            min="1"
-            max="50"
-            value={this.state.caracter}
-            onChange={this.handleRangeChange}
-            step="1"
-          />
-          {this.state.caracter}
-        </label>
-
-        {/* <button onClick={this.onSubmit}>Generar</button> */}
+        <ControlPanel>
+          <Generator valores={this.state} />
+          <label>
+            <input
+              name="minuscula"
+              type="checkbox"
+              checked={this.state.defaultMin}
+              onChange={this.handleMinChange}
+            ></input>
+            abc
+          </label>
+          <label>
+            <input
+              name="mayuscula"
+              type="checkbox"
+              checked={this.state.defaultMayus}
+              onChange={this.handleMayusChange}
+            ></input>
+            ABC
+          </label>
+          <label>
+            <input
+              name="numeros"
+              type="checkbox"
+              checked={this.state.defaultNum}
+              onChange={this.handleNumChange}
+            ></input>
+            123
+          </label>
+          <label>
+            <input
+              name="Caracteres"
+              type="checkbox"
+              checked={this.state.defaultSimbol}
+              onChange={this.handleSimbolChange}
+            ></input>
+            #!%
+          </label>
+          <label>
+            <input
+              id="typeinp"
+              type="range"
+              name="range"
+              min="1"
+              max="50"
+              value={this.state.caracter}
+              onChange={this.handleRangeChange}
+              step="1"
+            />
+            {this.state.caracter} Caracteres
+          </label>
+        </ControlPanel>
       </div>
     );
   }
--- src/Generator.jsx
+++ src/Generator.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import styled from "styled-components";
+import "./index.css";
 
 function Generator({ valores }) {
   var password = [];
@@ -6,30 +8,86 @@ function Generator({ valores }) {
   const valueMin = valores.defaultMin;
   const valueMayus = valores.defaultMayus;
   const valueNum = valores.defaultNum;
+  const valueSimbol = valores.defaultSimbol;
 
-  if (valueMin && valueMayus && valueNum) {
+  if (valueMin && valueMayus && valueNum && valueSimbol) {
+    password = valores.abc.concat(
+      valores.ABC,
+      valores.numbers,
+      valores.simbolos
+    );
+  } else if (valueMin && valueMayus && valueNum && !valueSimbol) {
     password = valores.abc.concat(valores.ABC, valores.numbers);
-  } else if (valueMin && valueMayus && valueNum === false) {
+  } else if (valueMin && valueMayus && !valueNum && !valueSimbol) {
     password = valores.abc.concat(valores.ABC);
-  } else if (valueMin && valueMayus === false && valueNum === false) {
+  } else if (valueMin && !valueMayus && !valueNum && !valueSimbol) {
     password = valores.abc.concat();
-  } else if (valueMin === false && valueMayus && valueNum) {
+  } else if (!valueMin && valueMayus && valueNum && valueSimbol) {
+    password = valores.ABC.concat(valores.numbers, valores.simbolos);
+  } else if (!valueMin && valueMayus && !valueNum && valueSimbol) {
+    password = valores.ABC.concat(valores.simbolos);
+  } else if (!valueMin && valueMayus && valueNum && !valueSimbol) {
     password = valores.ABC.concat(valores.numbers);
-  } else if (valueMin === false && valueMayus && valueNum === false) {
-    password = valores.ABC.concat();
-  } else if (valueMin && valueMayus === false && valueNum) {
-    password = valores.abc.concat(valores.numbers);
-  } else if (valueMin === false && valueMayus && valueNum) {
+  } else if (valueMin && !valueMayus && valueNum && valueSimbol) {
+    password = valores.abc.concat(valores.numbers, valores.simbolos);
+  } else if (!valueMin && valueMayus && valueNum && !valueSimbol) {
     password = valores.ABC.concat(valores.numbers);
-  } else if (valueMin === false && valueMayus === false && valueNum) {
+  } else if (!valueMin && !valueMayus && valueNum && valueSimbol) {
+    password = valores.numbers.concat(valores.simbolos);
+  } else if (!valueMin && !valueMayus && valueNum && !valueSimbol) {
     password = valores.numbers.concat();
+  } else if (!valueMin && valueMayus && !valueNum && !valueSimbol) {
+    password = valores.ABC.concat();
+  } else if (!valueMin && !valueMayus && !valueNum && valueSimbol) {
+    password = valores.simbolos.concat();
+  } else if (!valueMin && !valueMayus && valueNum && valueSimbol) {
+    password = valores.simbolos.concat(valores.numbers);
+  } else if (!valueMin && valueMayus && valueNum && valueSimbol) {
+    password = valores.simbolos.concat(valores.numbers, valores.ABC);
+  } else if (valueMin && !valueMayus && !valueNum && valueSimbol) {
+    password = valores.simbolos.concat(valores.abc);
+  } else if (valueMin && valueMayus && !valueNum && valueSimbol) {
+    password = valores.simbolos.concat(valores.abc, valores.ABC);
+  } else if (!valueMin && !valueMayus && !valueNum && !valueSimbol) {
+    password = ["h"];
   }
 
   for (let i = 0; i < valores.caracter; i++) {
-    const random = Math.floor(Math.random() * password.length) + 1;
+    const random = Math.floor(Math.random() * password.length);
     p[i] = password[random];
   }
 
-  return <h1>{p}</h1>;
+  const PasswordStyled = styled.div`
+    width: auto;
+    height: 50px;
+    border: 1px solid black;
+    background-color: white;
+    margin: auto;
+    padding: 0px;
+    justify-content: center;
+    p {
+      font-family: "Press Start 2P", cursive;
+      color: ${(props) =>
+        (valores.caracter <= 10 && "#e63e32") ||
+        (valores.caracter <= 19 && "#e6ce32") ||
+        (valores.caracter >= 20 && "#45c940")};
+    }
+  `;
+
+  return (
+    <>
+      <PasswordStyled>
+        <p>{p}</p>
+      </PasswordStyled>
+      <button
+        className="button-copy"
+        onClick={() => {
+          navigator.clipboard.writeText(p.join(""));
+        }}
+      >
+        Copiar
+      </button>
+    </>
+  );
 }
 export default Generator;
